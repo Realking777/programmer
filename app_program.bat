4d281e1bad7b0802850ffbc605d52f7a09c9edce
Realking <quartus@mail.ru>
2018-10-11 20:07:22 +0300

    SV Enable on start bug fixed

--- app_program.bat
+++ app_program.bat
@@ -2,7 +2,7 @@
 
 SET PATH=%PATH%D:\Altera\16.0\quartus\bin64;D:\Altera\16.0\qprogrammer\bin64\
 
-quartus_hps.exe -c 1 -o P -a 0x60000 iec61850.img.bin
+quartus_hps.exe -c 1 -o PV -a 0x60000 iec61850.img.bin
 
 pause
 
